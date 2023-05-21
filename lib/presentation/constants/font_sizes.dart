a6ceeeb4df74ef13a0c9922d0e09f39334655110
vladcto <razrab.ytka@gmail.com>
2023-05-21 23:48:15 +0300

    [DESIGN] Добавил константы отступов и шрифтов.

---
+++ lib/presentation/constants/font_sizes.dart
@@ -0,0 +1,6 @@
+sealed class FontSize {
+  static const double header = 28;
+  static const double large = 26;
+  static const double medium = 20;
+  static const double small = 18;
+}
---
+++ lib/presentation/constants/paddings.dart
@@ -0,0 +1,5 @@
+sealed class Paddings {
+  static const double large = 24;
+  static const double medium = 16;
+  static const double small = 8;
+}
