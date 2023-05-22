5de0c29d2fa5600f109448a979f509091bc2bd49
vladcto <razrab.ytka@gmail.com>
2023-05-22 21:18:14 +0300

    [FEATURE] Сделал набросок экрана настроек.

--- lib/presentation/pages/fragments/settings_fragment.dart
+++ lib/presentation/pages/fragments/settings_fragment.dart
@@ -1,3 +1,5 @@
+import 'package:blagodat/presentation/constants/paddings.dart';
+import 'package:blagodat/presentation/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
 class SettingsFragment extends StatelessWidget {
@@ -5,6 +7,35 @@ class SettingsFragment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: Colors.green);
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        // TODO: Header.
+        SizedBox(
+          height: 120,
+          child: Placeholder(),
+        ),
+        // TODO: Theme settings.
+        Expanded(
+          child: Center(
+            child: Placeholder(
+              child: SizedBox(
+                width: 152,
+                height: 165,
+              ),
+            ),
+          ),
+        ),
+        // TODO: Credits btn.
+        Padding(
+          padding: EdgeInsets.all(Paddings.medium),
+          child: Placeholder(
+            fallbackHeight: 50,
+          ),
+        ),
+        // Bottom bar space
+        SizedBox(height: MainPage.pageBarPadding),
+      ],
+    );
   }
 }
