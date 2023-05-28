b3f36540fd4519f1c56bd12c3e709ac99373648e
vladcto <razrab.ytka@gmail.com>
2023-05-29 00:01:07 +0300

    [FEATURE] Добавил список товаров.

---
+++ lib/data/shop/assortment/mock_assortment.dart
@@ -0,0 +1,27 @@
+import 'package:blagodat/data/shop/assortment/assortment.dart';
+import 'package:blagodat/data/shop/info/product.dart';
+
+/// Моковый класс для тестовой выборки продуктов.
+class MockAssortment implements Assortment {
+  static final _products = [
+    Product(
+      uid: _AutoId.id,
+      name: "Some",
+      imageURL:
+          "https://upload.wikimedia.org/wikipedia/commons/6/6b/Eastern_Orthodox_man_in_Jerusalem_by_David_Shankbone.jpg",
+      cost: 0.99,
+    ),
+  ];
+
+  @override
+  List<Product> get products => _products;
+}
+
+/// Класс предоставляющий сервис для получения UID.
+class _AutoId {
+  static int _i = 0;
+
+  static String get id {
+    return (_i++).toString();
+  }
+}
